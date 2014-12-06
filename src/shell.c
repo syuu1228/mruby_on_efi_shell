3715fc1beeabe299b407dad8640082bbc9f71d38
Takuya ASADA <syuu@dokukino.com>
2014-12-06 09:18:46 +0900

    Add Shell.gets

--- src/shell.c
+++ src/shell.c
@@ -3,7 +3,8 @@
 #include "mruby/data.h"
 #include "mruby/string.h"
 #include "error.h"
-#include  <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "uefi.h"
 
 mrb_value
@@ -19,6 +20,15 @@ mrb_shell_exec(mrb_state *mrb, mrb_value klass)
   return mrb_fixnum_value(ret);
 }
 
+mrb_value
+mrb_shell_gets(mrb_state *mrb, mrb_value klass)
+{
+  char buf[65535];
+  if (fgets(buf, 65535, stdin) == NULL)
+	return mrb_nil_value();
+  return mrb_str_new_cstr(mrb, buf);
+}
+
 void
 mrb_init_shell(mrb_state *mrb)
 {
@@ -27,5 +37,6 @@ mrb_init_shell(mrb_state *mrb)
   shell = mrb_define_class(mrb, "Shell", mrb->object_class);
   MRB_SET_INSTANCE_TT(shell, MRB_TT_DATA);
   mrb_define_class_method(mrb, shell, "exec", mrb_shell_exec, ARGS_REQ(1));
+  mrb_define_class_method(mrb, shell, "gets", mrb_shell_gets, ARGS_NONE());
 }
 
